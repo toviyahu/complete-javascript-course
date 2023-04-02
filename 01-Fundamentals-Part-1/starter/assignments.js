1631403a4fca7c725f54e4c649f343905f6df4c7
Ronke Oyekunle <ronke.lola@gmail.com>
2023-04-02 17:34:09 -0500

    Assignment complete vor values and variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,9 @@
+console.log("Test from Assignments.js");
+let country = "USA";
+let continent = "North America";
+let population = 336000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
