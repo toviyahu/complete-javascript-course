6e9f915d53746965f9af0b531680ff80d399438b
Ronke Oyekunle <ronke.lola@gmail.com>
2023-04-02 17:27:25 -0500

    Assignments file

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,11 @@
       if (js === 'amazing') alert('JavaScript is FUN!');
       40+8+23-10;
       console.log(40+8+23-10); 
+      console.log('Jonas');
+      console.log(23);
+
+      let firstName = "Matilda";
+      console.log(firstName);
+      console.log(firstName);
+      console.log(firstName);
+      
