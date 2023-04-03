f2ea643234dda557561722cd84f060831bb82d9e
Ronke Oyekunle <ronke.lola@gmail.com>
2023-04-02 19:00:54 -0500

    comparison operators and assigment operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -45,7 +45,27 @@ const birthYear = 1991;
 var job = 'programmer';
 job = 'business owner';
  */
-
-const ageJonas = 2037 - 1997;
-const ageSarah = 2037 - 2018;
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
 console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas /10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Yahu';
+console.log(firstName + ' ' + lastName);
+
+//Assignment Operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25 or x = 15 + 10 = 25
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+//Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
