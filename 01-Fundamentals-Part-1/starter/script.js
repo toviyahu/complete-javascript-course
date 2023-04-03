53fbc1b872743b8ad445d66ac9c6536cce82d993
Ronke Oyekunle <ronke.lola@gmail.com>
2023-04-03 02:05:27 -0500

    Code Challenge 1 and Strings and Template Literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -74,7 +74,7 @@ const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018); */
 
-const now = 2037;
+/* const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 
@@ -84,3 +84,56 @@ console.log(25 - 10 -5);
 let x, y;
 x = y = 25-10-5;
 console.log(x, y);
+
+//for multiple operators you have to use parenthesis for order of operations
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+ */
+/* 
+//Coding Challenge #1
+
+//Test Data 1 
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+markBMI = markMass / markHeight ** 2;
+console.log(markBMI);
+johnBMI = johnMass / (johnHeight * johnHeight);
+console.log(johnBMI);
+
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+//Test Data 2 
+const markMass2 = 95;
+const markHeight2 = 1.88;
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
+
+markBMI2 = markMass2 / markHeight2 ** 2 ;
+console.log(markBMI2);
+johnBMI2 = johnMass2 / (johnHeight2 * johnHeight2);
+console.log(johnBMI2);
+
+
+const markHigherBMI2 = markBMI2 > johnBMI2
+console.log(markHigherBMI2);
+  */
+
+//Strings and Template Literals
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ',a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string`);
