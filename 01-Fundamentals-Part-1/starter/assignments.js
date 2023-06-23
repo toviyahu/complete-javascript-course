bdd05dab7a8427aa3269900fdb17e137730cf91d
Ronke Oyekunle <ronke.lola@gmail.com>
2023-06-22 21:17:31 -0500

    adding assignment additions

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,9 +1,9 @@
-/* console.log("Test from Assignments.js");
+ console.log("Test from Assignments.js");
 let country = "USA";
 let continent = "North America";
-let population = 336000000;
+let population = 334233854;
 
 console.log(country);
 console.log(continent);
 console.log(population);
- */
+ 
