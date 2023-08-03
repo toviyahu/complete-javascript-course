a7626e4297eb6966c1971877e02cea599400082c
Ronke Oyekunle <ronke.lola@gmail.com>
2023-08-02 19:58:35 -0500

    Rest Pattern and Parameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -28,6 +28,11 @@ const restaurant = {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
   },
 
+  orderPizza: function(mainIngredient, ...otherIngredients){  //...otherIngredients is a REST operator
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+
 
   openingHours: {
     thu: {
@@ -45,7 +50,47 @@ const restaurant = {
   },
 };
 
-/////Spread Operator
+//1) Destructuring
+//SPREAD because on the right side of the = 
+const arr = [1, 2, ...[3, 4]];
+
+//REST because on the left side of the =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza, risotto, otherFood);
+
+//Objects
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(weekdays);
+
+//2) Functions
+
+const add = function(...numbers){
+//console.log(numbers);
+let sum = 0;
+for(let i = 0; i < numbers.length; i++) 
+sum += numbers[i];
+console.log(sum);
+}
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
+//rest operator is used in function declarations and function expressions (variables that hold functions)
+//spread operator is used in function calls and array literals
+
+////////////////////////////////////////////////
+
+/* /* /////Spread Operator
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -59,7 +104,7 @@ console.log(1, 2, 7, 8, 9);
 
 const newMenu = [...restaurant.mainMenu, 'Gnocci']; //adding new element to the array
 console.log(newMenu);
-/* spread operator is similar to destructing because it helps get elements out of the array, but it does not create new variables.  You can only use it in places where you would otherwise write values seperated by commas */
+// spread operator is similar to destructing because it helps get elements out of the array, but it does not create new variables.  You can only use it in places where you would otherwise write values seperated by commas 
 
 //Copy array
 const mainMenuCopy = [...restaurant.mainMenu];
@@ -91,10 +136,10 @@ console.log(newRestaurant);
 const restaurantCopy = {...restaurant};
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
-console.log(restaurant.name);
+console.log(restaurant.name); 
+ */
 
-/* 
-///////////////////////////////////////
+/*///////////////////////////////////////
 //Destructuring Objects
 const {name, openingHours, 
   categories} = restaurant;
